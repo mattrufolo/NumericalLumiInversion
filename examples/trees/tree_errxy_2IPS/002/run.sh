a962d7967370028f2fd10f53d5a8fed32509136a
mattrufolo <matteo.rufolo@outlook.it>
2024-03-19 15:30:08 +0100

    Revert "update"
    This reverts commit bcee205ccbaff91b13bf15fa443a0787cf65dc1c.

--- examples/trees/tree_errxy_2IPS/002/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source activate mruf_env
-cd /var/data/mrufolo/Inverting_luminosity/inv_gauss_tree_maker/000_errxy_good/002
-python 000_simple_job.py > output.txt 2> error.txt
--- examples/trees/tree_errxy_2IPS/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source activate mruf_env
-cd /var/data/mrufolo/Inverting_luminosity/inv_gauss_tree_maker/000_errxy_good
-python 000_simple_job.py > output.txt 2> error.txt
